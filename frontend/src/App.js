35a518c3852474a609721c5e10fd277e28c80dc2
chrstnale <christianale85@gmail.com>
2024-09-24 15:29:53 +0700

    feat(frontend): set up react as frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          User CRUD API
+      </header>
+    </div>
+  );
+}
+
+export default App;
